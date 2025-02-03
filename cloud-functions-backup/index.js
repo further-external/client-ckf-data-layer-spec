8f1466f52acb32f9f5a6a2727022e9d11ce7c593
jaime_herrera <121498633+JH-SDI@users.noreply.github.com>
2025-02-03 14:50:53 -0500

    Add files via upload

--- cloud-functions-backup/index.js
+++ cloud-functions-backup/index.js
@@ -1,13 +1,47 @@
-"use strict";
+/**
+ * Triggered from a change to a Cloud Storage bucket.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
 
-const { update_bing } = require("./update_bing");
-const { update_ttd } = require("./update_ttd");
-const { update_linkedin } = require("./update_linkedin");
+  
+const Storage = require('@google-cloud/storage');
+const storage = new Storage();
 
-require("dotenv").config();
+const {BigQuery} = require('@google-cloud/bigquery');
+const bigquery = new BigQuery();
 
-module.exports = {
-  update_bing,
-  update_ttd,
-  update_linkedin
+async function getFileData() {
+
+  const bucketName = 'ckf_campaign_manager';
+  const filePath = 'campaignManager.csv';
+  let contents = await storage.bucket(bucketName).file(filePath).download();
+  data = contents.toString()
+  let linesExceptFirst = data.split('\n')
+  let stopper = linesExceptFirst.length -2
+  linesExceptFirst = linesExceptFirst.slice(13,stopper)
+  let linesArr = linesExceptFirst.map(line=>line.split(','));
+  let vals = linesArr.map(x => {
+      keys = ["date", "campaign", "campaignId", "site", "placement", "placementId", "impressions", "clicks", "videoPlays", "videoCompletions", "cost"]
+      object = Object.fromEntries(keys.map((k, i) => [k, x[i]]));
+      return object;
+    })
+  return vals
+}
+
+const load_json = async function(datasetId, tableId, rows) {
+  await bigquery.dataset(datasetId)
+        .table(tableId)
+        .insert(rows)
+  console.log(`Inserted ${rows.length} rows`);    
+}
+
+exports.helloGCS = (event, context) => {
+  const gcsEvent = event;
+  console.log(`Processing file: ${gcsEvent.name}`);
+  getFileData(gcsEvent.name)
+    .then(x => load_json("109156790", "Campaign_Manager_Temp_II", x))
+    .catch(err => console.log(err))
+
 };
